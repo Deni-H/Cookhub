5430a24366447c4bba229d90e72574443194aaea
Deni-H <denyhilhamsyh@gmail.com>
2023-08-29 21:10:44 +0700

    Implementing UI on ForgetPasswordSuccessScreen

--- app/src/main/java/com/deni/hilhamsyah/cookhub/ui/auth_screen/forget_password/FogetPasswordSuccessScreen.kt
+++ app/src/main/java/com/deni/hilhamsyah/cookhub/ui/auth_screen/forget_password/FogetPasswordSuccessScreen.kt
@@ -1,12 +1,84 @@
 package com.deni.hilhamsyah.cookhub.ui.auth_screen.forget_password
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.deni.hilhamsyah.cookhub.R
+import com.deni.hilhamsyah.cookhub.navigation.Screen
+import com.deni.hilhamsyah.cookhub.ui.theme.CookhubTheme
+import com.deni.hilhamsyah.cookhub.ui.theme.neutral50
 
 @Composable
 fun ForgetPasswordSuccessScreen(
     navController: NavController
 ) {
-    Text(text = "Forget Password Success Screen")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            imageVector = ImageVector.vectorResource(R.drawable.success_mark),
+            contentDescription = "success_mark"
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            "Email sent!",
+            style = MaterialTheme.typography.titleMedium
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(
+            "Verification url has been sent\n to your email.",
+            textAlign = TextAlign.Center,
+            color = neutral50
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            modifier = Modifier
+                .height(50.dp)
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(10.dp),
+            onClick = {
+                navController.popBackStack()
+                navController.navigate(Screen.LoginScreen.route)
+            }
+        ) {
+            Text(
+                text = "Back to login",
+                style = MaterialTheme.typography.bodyMedium,
+                fontWeight = FontWeight.Medium
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ForgetPasswordSuccessScreenPreview() {
+    CookhubTheme {
+        ForgetPasswordSuccessScreen(navController = rememberNavController())
+    }
 }
