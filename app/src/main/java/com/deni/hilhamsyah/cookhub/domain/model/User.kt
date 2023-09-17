fade6627ce8159f0d4eba3a3abad4cd3ee22669a
Deni-H <denyhilhamsyh@gmail.com>
2023-09-18 00:40:35 +0700

    Adding UserName data class on User.kt

--- app/src/main/java/com/deni/hilhamsyah/cookhub/domain/model/User.kt
+++ app/src/main/java/com/deni/hilhamsyah/cookhub/domain/model/User.kt
@@ -1,11 +1,32 @@
 package com.deni.hilhamsyah.cookhub.domain.model
 
+import com.google.gson.annotations.SerializedName
+
 data class User(
-    val id: String,
-    val email: String,
-    val userName: String?,
-    val firstName: String,
+
+    @SerializedName("email")
+    val email: String?,
+
+    @SerializedName("user_name")
+    val userName: UserName?,
+
+    @SerializedName("first_name")
+    val firstName: String?,
+
+    @SerializedName("last_name")
     val lastName: String?,
+
+    @SerializedName("bio")
     val bio: String?,
+
+    @SerializedName("profile_image")
     val profileImage: String?
+)
+
+data class UserName(
+    @SerializedName("value")
+    val value: String?,
+
+    @SerializedName("last_changed")
+    val lastChanged: Long
 )
