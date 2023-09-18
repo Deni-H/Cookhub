9d4b83b3e34867b0c9960e1578d0dcc6c87d195a
Deni-H <denyhilhamsyh@gmail.com>
2023-09-19 04:37:20 +0700

    Adding SerializedName annotation

--- app/src/main/java/com/deni/hilhamsyah/cookhub/domain/model/Recipe.kt
+++ app/src/main/java/com/deni/hilhamsyah/cookhub/domain/model/Recipe.kt
@@ -2,14 +2,47 @@ package com.deni.hilhamsyah.cookhub.domain.model
 
 import com.deni.hilhamsyah.cookhub.util.TimeUnit
 import com.deni.hilhamsyah.cookhub.util.WeightUnit
+import com.google.gson.annotations.SerializedName
 
 data class Recipe(
-    val id: String,
-    val userId: String,
+
+    @SerializedName("creator")
+    val creator: String?,
+
+    @SerializedName("title")
     val title: String,
+
+    @SerializedName("description")
     val description: String,
+
+    @SerializedName("image")
     val image: String,
+
+    @SerializedName("video")
     val video: String?,
-    val ingredients: List<Pair<String, Pair<Int, WeightUnit>>>,
-    val cookTime: Pair<Int, TimeUnit>
+
+    @SerializedName("created_at")
+    val createdAt: Long,
+
+    @SerializedName("cook_time")
+    val cookTime: CookTime,
+
+    @SerializedName("ingredients")
+    val ingredients: Map<String, Ingredient>
+)
+
+data class CookTime(
+    @SerializedName("time_unit")
+    val timeUnit: TimeUnit,
+
+    @SerializedName("value")
+    val value: Int
+)
+
+data class Ingredient(
+    @SerializedName("weight_unit")
+    val weightUnit: WeightUnit,
+
+    @SerializedName("value")
+    val value: Int
 )
