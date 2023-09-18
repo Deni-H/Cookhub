d404b4748e802a98383d66aa2652bdd48ad4d3f0
Deni-H <denyhilhamsyh@gmail.com>
2023-09-19 04:32:52 +0700

    Adding message on Resource.Error

--- app/src/main/java/com/deni/hilhamsyah/cookhub/util/Resource.kt
+++ app/src/main/java/com/deni/hilhamsyah/cookhub/util/Resource.kt
@@ -3,5 +3,8 @@ package com.deni.hilhamsyah.cookhub.util
 sealed class Resource<out T> {
     data class Loading<out T>(val data: T? = null) : Resource<T>()
     data class Success<out T>(val data: T) : Resource<T>()
-    data class Error<out T>(val e: Throwable, val data: T? = null) : Resource<T>()
+    data class Error<out T>(
+        val e: Throwable? = null,
+        val message: ErrorMessage? = null
+    ) : Resource<T>()
 }
