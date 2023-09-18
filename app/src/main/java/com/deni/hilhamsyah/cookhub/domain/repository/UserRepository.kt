9adbfcb74207ffa1d98eb40325b973195772b39b
Deni-H <denyhilhamsyh@gmail.com>
2023-09-19 04:39:06 +0700

    Adding UserRepository.kt

---
+++ app/src/main/java/com/deni/hilhamsyah/cookhub/domain/repository/UserRepository.kt
@@ -0,0 +1,35 @@
+package com.deni.hilhamsyah.cookhub.domain.repository
+
+import com.deni.hilhamsyah.cookhub.domain.model.Uid
+import com.deni.hilhamsyah.cookhub.domain.model.User
+import com.deni.hilhamsyah.cookhub.domain.model.response.CheckUserNameResponse
+import com.deni.hilhamsyah.cookhub.domain.model.response.IsFollowingResponse
+import com.deni.hilhamsyah.cookhub.domain.model.response.SuccessResponse
+import com.deni.hilhamsyah.cookhub.util.Resource
+import kotlinx.coroutines.flow.Flow
+
+interface UserRepository {
+    fun getUserProfile(): Flow<Resource<User>>
+
+    fun addUserProfile(user: User): Flow<Resource<SuccessResponse>>
+
+    fun updateUserProfile(user: User): Flow<Resource<User>>
+
+    fun getUserProfileById(userId: String): Flow<Resource<User>>
+
+    fun getUserFollowers(userId: String): Flow<Resource<List<Uid>>>
+
+    fun getUserFollowing(userId: String): Flow<Resource<List<Uid>>>
+
+    fun getUserRecipes(userId: String): Flow<Resource<List<Uid>>>
+
+    fun followUser(userId: String): Flow<Resource<SuccessResponse>>
+
+    fun unfollowUser(userId: String): Flow<Resource<SuccessResponse>>
+
+    fun isFollowing(userId: String): Flow<Resource<IsFollowingResponse>>
+
+    fun setUserName(userName: Map<String, String>): Flow<Resource<SuccessResponse>>
+
+    fun isUserNameExists(userName: String): Flow<Resource<CheckUserNameResponse>>
+}
