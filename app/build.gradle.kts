12f210958e71d6674e57d1328e2e7bcf8646f467
Deni-H <denyhilhamsyh@gmail.com>
2023-09-25 08:19:22 +0700

    Adding Coil dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -109,4 +109,7 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 
+    // Coil
+    implementation("io.coil-kt:coil-compose:2.4.0")
+
 }
