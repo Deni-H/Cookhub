80f05ee14a987b2af7a66c1b87d230a57387c9b6
Deni-H <denyhilhamsyh@gmail.com>
2023-09-28 22:53:28 +0700

    provide saveOnBoardingState and readOnBoardingState

--- app/src/main/java/com/deni/hilhamsyah/cookhub/di/DatastoreModule.kt
+++ app/src/main/java/com/deni/hilhamsyah/cookhub/di/DatastoreModule.kt
@@ -3,6 +3,8 @@ package com.deni.hilhamsyah.cookhub.di
 import android.content.Context
 import com.deni.hilhamsyah.cookhub.data.local.DatastoreRepositoryImpl
 import com.deni.hilhamsyah.cookhub.domain.repository.DatastoreRepository
+import com.deni.hilhamsyah.cookhub.domain.use_case.ReadOnBoardingState
+import com.deni.hilhamsyah.cookhub.domain.use_case.SaveOnBoardingState
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -18,5 +20,21 @@ object DatastoreModule {
     @Singleton
     fun provideDataStore(@ApplicationContext context: Context): DatastoreRepository {
         return DatastoreRepositoryImpl(context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideSaveOnBoardingState(
+        datastoreRepository: DatastoreRepository
+    ): SaveOnBoardingState {
+        return SaveOnBoardingState(datastoreRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideReadOnBoardingState(
+        datastoreRepository: DatastoreRepository
+    ): ReadOnBoardingState {
+        return ReadOnBoardingState(datastoreRepository)
     }
 }
