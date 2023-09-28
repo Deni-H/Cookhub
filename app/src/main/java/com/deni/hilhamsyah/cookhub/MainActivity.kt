23329e46fd450a3731a85fdb6878e133e98e6aed
Deni-H <denyhilhamsyh@gmail.com>
2023-09-28 22:52:54 +0700

    Adding initialize App check on onCreate

--- app/src/main/java/com/deni/hilhamsyah/cookhub/MainActivity.kt
+++ app/src/main/java/com/deni/hilhamsyah/cookhub/MainActivity.kt
@@ -7,6 +7,11 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.deni.hilhamsyah.cookhub.navigation.NavGraph
 import com.deni.hilhamsyah.cookhub.ui.theme.CookhubTheme
+import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory
+import com.google.firebase.appcheck.ktx.appCheck
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.initialize
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,11 +20,26 @@ class MainActivity : ComponentActivity() {
     private lateinit var navHostController: NavHostController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initDebug()
         setContent {
             CookhubTheme {
                 navHostController = rememberNavController()
                 NavGraph(navHostController = navHostController)
             }
         }
+    }
+
+    private fun init() {
+        Firebase.initialize(context = this)
+        Firebase.appCheck.installAppCheckProviderFactory(
+            PlayIntegrityAppCheckProviderFactory.getInstance(),
+        )
+    }
+
+    private fun initDebug() {
+        Firebase.initialize(context = this)
+        Firebase.appCheck.installAppCheckProviderFactory(
+            DebugAppCheckProviderFactory.getInstance(),
+        )
     }
 }
